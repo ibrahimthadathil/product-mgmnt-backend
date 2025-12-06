00b78d1c5e3960e55fbf5852f1411f827bf2cd62
ibrahim <ibrahiminfo8@gmail.com>
2025-12-06 19:53:20 +0530

    initial commit with server setup

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/DB_config";
+
+dotenv.config();
+connectDB();
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "API working 🚀" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port  http://localhost:${PORT}`);
+});
